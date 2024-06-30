2242b1027f50575bac0fe90c0fc650993346035a
PRIYANSH-2004 <priyanshsingh.pr@gmail.com>
2024-06-30 10:22:10 +0530

    chages in navbar + requirements.xt

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import numpy as np
 import pickle
 
 popular_df = pickle.load(open('popular_df.pkl', "rb"))
+pt = pickle.load(open('pt.pkl', "rb"))
+books = pickle.load(open('books.pkl', "rb"))
+similarity_score = pickle.load(open('similarity_score.pkl', "rb"))
 app = Flask(__name__)
 
 
@@ -14,6 +18,38 @@ def index():
                            votes = list(popular_df['num_ratings'].values),
                            rating = list(popular_df['avg_rating'].values),
                            )
+
+
+@app.route('/recommend_books', methods=['post'])
+def recommend():
+    user_input = request.form.get('user_input')
+    n=10
+    
+    index= np.where(pt.index==user_input)[0][0]
+    distances = similarity_score[index]
+    # enumerates gin=ves index in list
+    # lambda function for sorting on basis of distances not on index
+    # we took top n books
+    similar_items = sorted(list(enumerate(distances)), key=lambda x:x[1], reverse=True)[1:n+1]
+    data =[]
+    for it in similar_items:
+        item=[]
+        # print(it[0], "  ->  " , pt.index[it[0]])
+        temp_df = books[books['Book-Title'] == pt.index[it[0]]]
+        # no duplicate books
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+        data.append(item)
+
+    # data has been paased to recoomed.html
+    # return data
+    return render_template('recommend.html', data=data)
+
+    # print(data)
+
+    # return str(user_input)
+
 
 
 @app.route('/recommend')
